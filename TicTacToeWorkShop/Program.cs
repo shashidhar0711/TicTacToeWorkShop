2366a2796fd7493756579b6444acf73c14cc0061
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-06 17:50:43 +0530

    Add project files.

---
+++ TicTacToeWorkShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacToeWorkShop
+{
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tictactoe workshop");
+        }
+    }
+}
